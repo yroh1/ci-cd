53f222f5d9a8d6dcdaaff7c79c7d64cfbdcc3491
Micha Bitton <micha.bitton@biocatch.com>
2023-03-24 13:16:18 +0300

    adding few steps

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -13,7 +13,7 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("cd basics"; "npm install"; "tar -czvf myapp.tar.gz .")
+        shell("""cd basics; npm install; tar -czvf myapp.tar.gz .""")
     }
 
     publishers {
