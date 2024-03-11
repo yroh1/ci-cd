ef22c175372f1dea51131c987612aa8a494de9ef
yroh1 <147074609+yroh1@users.noreply.github.com>
2024-03-11 15:10:21 +0200

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/micha-bitton/ci-cd.git', 'main') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/yroh1/ci-cd.git', 'main') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL NodeJs User Example')
             node / gitConfigEmail('jenins-dsl@domain.com')
         }
